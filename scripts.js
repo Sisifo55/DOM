a66f299d5bb014ada007ef87bf06bf7762859737
Sergio Mariño <sergiusmaf@gmail.com>
2024-05-08 19:58:07 +0200

    subir cambios del ejercicio

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ let title = document.getElementById("gen-1");
 title.innerHTML = "Generasión 1 Pokimon";
 
 //EJERCICIO 2
-let backgroundChange = document.getElementsByClassName("infocard-list infocard-list-pkmn-lg");
-backgroundChange.style.background = "yellow";
+const infocard = document.querySelector('.infocard-list');
+infocard.style.background = 'yellow';
 
 //EJERCICIO 3
 console.log(window.location.href);
@@ -19,4 +19,12 @@ for (let i = 0; i < imagenes.length; i++) { console.log(imagenes[i].src);};
 //EJERCICIO 6"
 let imagenes2 = document.querySelectorAll("img");
 for (let i = 0; i < imagenes2.length; i++) { imagenes2[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";};
+
+//EJERCICIO 7/ 
+let infoCards = document.querySelectorAll(".infocard-lg-data.text-muted");
+for ( i = 0; i < infoCards.length; i++) {
+if (infoCards[i].innerHTML.includes("flying")){
+infoCards[i].style.backgroundColor = "lightblue";
+}
+}
 
